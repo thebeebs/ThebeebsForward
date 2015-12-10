65becc723e9b1fcbcc34fb652413808b48336389
Martin Beeby <martinbeeby@hotmail.com>
2015-12-10 15:04:23 +0000

    Adding Forward

---
+++ src/ThebeebsForward/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace ThebeebsForward
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                context.Response.Redirect("https://blogs.msdn.microsoft.com/thebeebs/");
+                await context.Response.WriteAsync("Forwarding to https://blogs.msdn.microsoft.com/thebeebs/");
+            });
+        }
+    }
+}
